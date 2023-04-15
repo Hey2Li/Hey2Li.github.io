04d786d2eee6fbce022eaa108d04d230019c493f
litao <litao@cooplan.cn>
2023-04-15 20:54:20 +0800

    Site updated: 2023-04-15 20:54:20

--- js/duration.js
+++ js/duration.js
@@ -1,6 +1,6 @@
 !(function() {
     /** 计时起始时间，自行修改 **/
-    var start = new Date("2016/07/13 00:00:00");
+    var start = new Date("2023/04/13 00:00:00");
 
     function update() {
         var now = new Date();
